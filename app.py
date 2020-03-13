1840a5a8e3b5761be0c59340adb3a0356d879ad0
Jeremiah <546108612@qq.com>
2020-03-13 23:57:47 +0800

    Add 404 page

--- app.py
+++ app.py
@@ -105,3 +105,9 @@ def test_url_for():
     return 'Test page'
 
 
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+
